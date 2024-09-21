3e9f36819c6cef844c3ff446ee5a49ee86c68ca7
Naveena-15 <naveenanaveena07805@gmail.com>
2024-09-21 09:21:19 +0530

    event and dailycode final

--- app/(main)/dailyquote/page.tsx
+++ app/(main)/dailyquote/page.tsx
@@ -14,3 +14,19 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
+// "use client";
+// import React from 'react';
+// import { CalendarWithDailyquote } from '../../../components/actions/dailyquotes';
+
+// const HomePage: React.FC = () => {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+//       <div className="bg-white p-6 rounded-lg shadow-lg w-full">
+//         {/* <h1 className="text-2xl font-bold mb-4 text-center">Daily Quotes Calendar</h1> */}
+//         <CalendarWithDailyquote />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
